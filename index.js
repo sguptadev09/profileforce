b7c7ccfc9ef9bb3b988c04a0cdaf9b87f479077c
sgupta9591 <sgupta9591@gmail.com>
2019-09-15 14:46:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-var http = require('http');
+var http = require("http");
 var fs = require("fs");
-var url  = require('url');
+var url  = require("url");
 
-const htmlData = fs.readFileSync("index.html");
+const htmlData = fs.readFileSync("index.html", "utf8");
 const appUrl = "https://sf-devs-developer-edition.ap15.force.com/profileforce";
 
 http.createServer(
 	function(request, response) {
-	  	response.writeHead(200, {'Content-Type': 'text/html'});
+	  	response.writeHead(200, {"Content-Type": "text/html"});
 	  	response.write(htmlData.replace("{URL}", appUrl + url.parse(request.url).pathname));
 	  	response.end();
 	}
