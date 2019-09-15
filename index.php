e35a8858959aa74739d36e7d40dcbd11cbb2af0c
sgupta9591 <sgupta9591@gmail.com>
2019-09-15 14:18:29 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,11 +4,16 @@
 	    <title>Profileforce</title>
 	    <script>
 	    	window.onload = function(){
-	    		document.getElementById("profileforce").src = "https://sf-devs-developer-edition.ap15.force.com/profileforce" + location.pathname + location.search; 
+	    		let h = window.innerHeight;
+  				let w = window.innerWidth;
+  				let iframe = document.getElementsByTagName("iframe")[0];
+				iframe.style.height = h + "px";
+				iframe.style.width = w + "px";
+	    		iframe.src = "https://sf-devs-developer-edition.ap15.force.com/profileforce" + location.pathname + location.search; 
 	    	}
 		</script>
 	</head>
 	<body style="margin:0px;padding:0px;overflow:hidden">
-	    <iframe id="profileforce" src="" frameborder="0" style="overflow:hidden;height:100%;width:100%" height="100%" width="100%"></iframe>
+	    <iframe src="" frameborder="0" style="overflow:hidden;height:100%;width:100%" height="100%" width="100%"></iframe>
 	</body>
 </html>
