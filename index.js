f49c082be461ec64b57dba597aedaa2ffc0065b9
Profileforce <sforcedevs@gmail.com>
2019-10-16 19:26:21 +0530

    test

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 var httpProxy = require('http-proxy');
 
-httpProxy.createProxyServer({target:'http://sf-devs-developer-edition.ap15.force.com'})
+httpProxy.createProxyServer({target:'https://sf-devs-developer-edition.ap15.force.com'})
 .listen(process.env.PORT || 8080);
