b087093ed45bcd23d4ceb6ed589e508e97524080
Profileforce <sforcedevs@gmail.com>
2019-10-17 09:31:54 +0530

    test

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var url = require('url');
 
 proxyServer = proxy.createProxyServer({ target: process.env.APP_URL });
 
-//proxyServer.listen(8000);
+proxyServer.listen(process.env.PORT);
 
 server = http.createServer(function (req, res) {
 
@@ -22,4 +22,4 @@ server = http.createServer(function (req, res) {
 
 });
 
-server.listen(process.env.PORT || 8080);
+//server.listen(process.env.PORT || 8080);
