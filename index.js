b6a1edb2c352858b1cedd8ab5c8ba6ade7a3b237
Profileforce <sforcedevs@gmail.com>
2019-10-24 03:04:47 +0530

    test

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const app = express();
 /*process.env.APP_URL = 'http://sf-devs-developer-edition.ap15.force.com';
 process.env.PORT = 8080;*/
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/resource/*/SLDS', express.static(path.join(__dirname, 'public/slds')));
 
 app.all('*', function(req, res, next){
-	if (req.url.startsWith('/_slds')) {
+	if (req.url.includes('SLDS')) {
 		next();
 	} else {
 		var appUrl = process.env.APP_URL + url.parse(req.url).pathname;
