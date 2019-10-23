09808ddfb1fef75cedeb438285bdbb1ffdabde6e
Profileforce <sforcedevs@gmail.com>
2019-10-24 01:30:02 +0530

    test

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ process.env.PORT = 8080;*/
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.all('*', function(req, res, next){
+	console.log(req.url, req.url.startsWith('/_slds'));
 	if (req.url.startsWith('/_slds')) {
 		return next();
 	}
