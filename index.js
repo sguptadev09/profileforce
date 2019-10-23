fba859144e3ae2f3e73180ad9a93892a4dbb7ebf
Profileforce <sforcedevs@gmail.com>
2019-10-24 02:15:07 +0530

    test

--- index.js
+++ index.js
@@ -10,15 +10,6 @@ process.env.PORT = 8080;*/
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(function(req, res, next) {
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization,X-Authorization'); 
-	res.setHeader('Access-Control-Allow-Methods', '*');
-	res.setHeader('Access-Control-Expose-Headers', 'X-Api-Version, X-Request-Id, X-Response-Time');
-	res.setHeader('Access-Control-Max-Age', '1000');  
-	next();
-});
-
 app.all('*', function(req, res, next){
 	if (req.url.startsWith('/_slds')) {
 		next();
